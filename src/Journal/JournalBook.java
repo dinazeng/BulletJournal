b529f382de4063a52b426e99d512662959d66af3
Dina Zeng <dina.zeng22@gmail.com>
2022-01-28 11:12:30 -0800

    created class to manage Entry objects under package Journal

---
+++ src/Journal/JournalBook.java
@@ -0,0 +1,13 @@
+package Journal;
+
+import JournalPages.Entry;
+
+import java.util.ArrayList;
+
+public class JournalBook {
+
+    ArrayList<Entry> bookPages = new ArrayList<Entry>();
+
+    public JournalBook() {
+    }
+}
